50099d820ff27f1cb4eceed071396420e4563abf
Juan Ignacio Marchetto <juanignaciomarchetto@gmail.com>
2023-11-21 12:24:29 -0300

    21/11/2023-12:24

--- src/components/ContadorResponsive/ContadorResponsive.jsx
+++ src/components/ContadorResponsive/ContadorResponsive.jsx
@@ -23,27 +23,45 @@ const ContadorResponsive = () => {
     const mostrarTextoTope = contador === 0 || contador === 20;
 
     return (
-        <Box textAlign="center" mt={8} bg="#0ed1c7"p={6} width="60%" height="300px" display="flex" alignItems="center" justifyContent="center"> 
-            <Button
-                onClick={incrementarContador}
-                disabled={contador >= 20}
-                mr={isLargerThan768 ? 2 : 0}
-                bg="#722856"
-            >
-                Sumar +
-            </Button>
-            <span>{contador}</span>
-            <Button
-                onClick={decrementarContador}
-                disabled={contador <= 0}
-                ml={isLargerThan768 ? 2 : 0}
-                bg="#722856"
-            >
-                Restar -
-            </Button>
-            <div></div>
+        <Box
+            textAlign="center"
+            mt={8}
+            bg="#0ed1c7"
+            p={6}
+            width="60%"
+            height="300px"
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            justifyContent="center"
+        >
+            <Box mb={4}>
+                <Button
+                    onClick={incrementarContador}
+                    disabled={contador >= 20}
+                    mr={isLargerThan768 ? 2 : 0}
+                    bg="#722856"
+                    p={3}
+                >
+                    Sumar +
+                </Button>
+                <span p={1} background="#09524e">{contador}</span>
+                <Button
+                    onClick={decrementarContador}
+                    disabled={contador <= 0}
+                    ml={isLargerThan768 ? 2 : 0}
+                    bg="#722856"
+                    p={3}
+                >
+                    Restar -
+                </Button>
+            </Box>
             {mostrarTextoTope && (
-                <p>{contador === 0 ? 'Tope mínimo!' : '¡Tope máximo!'}</p>
+                <p style={{
+                    background: '#09524e',
+                    padding: '2px',
+                    borderRadius: '5px',
+                }}>{contador === 0 ? 'Tope mínimo!' : '¡Tope máximo!'}</p>
             )}
         </Box>
     );
