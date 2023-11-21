2320eea9088b87b3ac4d8ed1bd8e40224dc47520
Juan Ignacio Marchetto <juanignaciomarchetto@gmail.com>
2023-11-21 08:45:11 -0300

    21/11/2023-08:45

--- src/components/contador/contador.jsx
+++ src/components/contador/contador.jsx
@@ -2,16 +2,16 @@ const app = document.querySelector('.app');
 const useState = React.useState
 
 
-function Contador(){
-  let [value, setValue] = useState(0)
-  let [times, setTimes] = useState(0)
-  
-  return <React.Fragment>
-           <button onClick={()=>{setValue(value+1); setTimes(times + 1)}}> incrementar + </button>
-           <span>{value}</span>
-           <button onClick={()=>{setValue(value-1); setTimes(times + 1)}}button> decrementar - </button>
-           <div>: {times}</div>
-        </React.Fragment>;    
+function Contador() {
+    let [value, setValue] = useState(0)
+    let [times, setTimes] = useState(0)
+
+    return <React.Fragment>
+        <button onClick={() => { setValue(value + 1); setTimes(times + 1) }}> incrementar + </button>
+        <span>{value}</span>
+        <button onClick={() => { setValue(value - 1); setTimes(times + 1) }} button> decrementar - </button>
+        <div>: {times}</div>
+    </React.Fragment>;
 }
 
-ReactDOM.render(<Contador/>, app)
+ReactDOM.render(<Contador />, app)
